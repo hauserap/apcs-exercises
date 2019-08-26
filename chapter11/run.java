002a4a0287ae67eb4ac25f7ed6538fd9a3c6069c
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-26 12:47:47 -0600

    add str2

--- chapter11/run.java
+++ chapter11/run.java
@@ -1,9 +1,10 @@
 public class run{
     public static void main (String [] args){
         String str ;
+        String str2;
 
         str = new String ("hello world");
-        str2 = new String (" goodbye world");
+        str2 = new String ("goodbye world");
 
         System.out.println (str);
         System.out.println(str2);
