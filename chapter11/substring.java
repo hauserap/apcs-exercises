72529a3a6ef6549946f3332c9ae29c07b4525fae
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-27 12:23:51 -0600

    finish sub string

---
+++ chapter11/substring.java
@@ -0,0 +1,12 @@
+public class substring
+{
+  public static void main ( String[] args )
+  {
+    String str = new String( "Golf is a good walk spoiled." ); 
+
+    String sub = str.substring(18,22); 
+
+    System.out.println( sub );
+
+  }
+}
