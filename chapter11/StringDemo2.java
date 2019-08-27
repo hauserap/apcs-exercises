628630ad3247f5741ea3b11d0c572b120aa47bea
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-27 12:10:52 -0600

    finish string demo 2

--- chapter11/StringDemo2.java
+++ chapter11/StringDemo2.java
@@ -1,5 +1,15 @@
-public class StringDemo2{
-    public static void main (String [] args){
-        
-    }
+public class StringDemo2
+{
+  public static void main ( String[] args )
+  {
+    String str;
+    int    len;
+
+    str = new String( "aight bro im boutta head out" );
+
+    len = str.length();
+
+    System.out.println("The length is: " + len );
+
+  }
 }
