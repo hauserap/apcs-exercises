793d03790cab124f5c93f945272c6d54a17ec724
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-28 12:52:56 -0600

    fix hom law

--- chapter12/Ohm.java
+++ chapter12/Ohm.java
@@ -4,14 +4,14 @@ public class ohm{
     public static void main(String [] args){
         Scanner scan = new Scanner (System.in);
         Scanner scan2 = new Scanner (System.in);
-        System.out.println("enter V variable");
+        System.out.println("enter Voltage variable");
         double v = scan.nextInt();
-        System.out.println("enter r variable");
+        System.out.println("enter resistance variable");
         double r = scan2.nextInt();
 
         
 
-        double amps = v/r;
+        double amps = (v + 0.0)/r;
 
         System.out.println("amps is" + amps);
 
