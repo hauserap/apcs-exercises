bf59101d6d15dc9720324a2935d31930e4aad649
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-28 12:50:36 -0600

    finish ohm law

--- chapter12/Ohm.java
+++ chapter12/Ohm.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
 public class ohm{
-    
+    public static void main(String [] args){
+        Scanner scan = new Scanner (System.in);
+        Scanner scan2 = new Scanner (System.in);
+        System.out.println("enter V variable");
+        double v = scan.nextInt();
+        System.out.println("enter r variable");
+        double r = scan2.nextInt();
+
+        
+
+        double amps = v/r;
+
+        System.out.println("amps is" + amps);
+
+
+
+
+    }
 }
