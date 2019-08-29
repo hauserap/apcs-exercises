526ecca92ee835bd798386da6bcb5de40b1e7d9d
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-29 14:57:53 -0600

    finish funsub

--- chapter12/FunSub.java
+++ chapter12/FunSub.java
@@ -1,7 +1,25 @@
 import java.util.Scanner;
 
-public class Funsub{
-    public static void main(String []args);
+public class FunSub
+{
+    public static void main (String [] args){
+
+        String str;
+        String sub;
+
+        str = new String ("Eva, can I stab bats in a cave?");
+
+        sub = str.substring(2,8);
+
+        System.out.println(sub);
+
+
+
+
+    }
+
+
+
 
 
 }
