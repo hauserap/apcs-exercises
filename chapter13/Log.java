980365ffa44f20e21d113da07d275c94bc3f1322
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-29 16:50:55 -0600

    finish log

---
+++ chapter13/Log.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Log{
+    public static void main (String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a double");
+        double x = scan.nextFloat();
+        float equal = Math.log()=Math.log(2);
+
+        System.out.println("Base 2 log is");
+
+
+
+    }
+
+}
