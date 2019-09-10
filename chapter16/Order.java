a23af8b8de6fa101c653a894b86c18d9641fcc4e
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-10 12:49:08 -0600

    Start order

---
+++ chapter16/Order.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Order{
+    public static void main(String [] args){
+        Scanner scan = new Scanner (System.in);
+
+        int bolt;
+        int nut;
+        int washer;
+
+        System.out.println("Enter number of bolts");
+        bolt = scan.nextInt();
+        System.out.println("Enter number of nuts");
+        nut = scan.nextInt();
+        System.out.println("Enter number of washers");
+        washer = scan.nextInt();
+
+    }
+
+
+}
