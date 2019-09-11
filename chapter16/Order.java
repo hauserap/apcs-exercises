35261e68f175ee9796a634661db08a3bdf045a16
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-11 13:09:13 -0600

    finish order

--- chapter16/Order.java
+++ chapter16/Order.java
@@ -8,12 +8,29 @@ public class Order{
         int nut;
         int washer;
 
+        int boltCost = 5;
+        int nutCost = 3;
+        int washerCost =1;
+
         System.out.println("Enter number of bolts");
         bolt = scan.nextInt();
         System.out.println("Enter number of nuts");
         nut = scan.nextInt();
         System.out.println("Enter number of washers");
         washer = scan.nextInt();
+
+        if (washer*2<= bolt){
+            System.out.println("too few washers");
+        } else if (nut<=bolt) {
+            System.out.println("too few nuts");
+        } else {
+            System.out.println(" looks good");
+        }
+
+        double orderCost = (bolt*boltCost)+(nut*nutCost)+(washer*washerCost);
+        System.out.println("Order Cost:" + orderCost);
+
+
 
     }
 
