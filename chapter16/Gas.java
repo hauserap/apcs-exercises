676a32fa2f2f9ac6e6c1ba512d41f18a29eb57d7
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-13 12:09:05 -0600

    finish gas

--- chapter16/Gas.java
+++ chapter16/Gas.java
@@ -1,7 +1,32 @@
-import java.util.Scanner; 
+import java.util.Scanner;
+public class Gas {
+    public static void main(String[] args) {
+        String gasTank;
+        gasTank = new String("Insert Tank capacity");
+        Scanner scan = new Scanner(System.in);
+        System.out.println(gasTank);   
+        String inData = scan.nextLine();
+        Integer totalGas = Integer.parseInt(inData);
+        int gasTotal = totalGas;
 
-public class Gas{
-    public static void main (String [] args){
+        String gasPercent;
+        gasPercent = new String("Insert Gage reading in percent");
+        System.out.println(gasPercent);   
+        String inData1 = scan.nextLine();
+        Integer totalPercent = Integer.parseInt(inData1);
+        int gageTotalFinal = totalPercent;
         
+        String MPG;
+        MPG = new String("Insert Miles per gallon of your car");
+        System.out.println(MPG);   
+        String inData2 = scan.nextLine();
+        Integer totalMPG = Integer.parseInt(inData2);
+        int mPGTotal = totalMPG;
+        double gasFinal = ((gageTotalFinal * .01) * gasTotal) * mPGTotal;
+
+        if(gasFinal < 200) 
+        System.out.println("Get Gas!");
+        else
+        System.out.println("You are safe for departure!");
     }
 }
