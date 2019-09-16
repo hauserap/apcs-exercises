8a6959733268a4c8bafbaec6b385904128cfb6dc
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-16 12:48:59 -0600

    finish chapter18

--- chapter18/Charge.java
+++ chapter18/Charge.java
@@ -1,2 +1,22 @@
 import java.util.Scanner;
 
+public class Charge{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insert you checking account balance");
+
+        int checking = scan.nextInt();
+        System.out.println("Your checking balance is  " + checking);
+        System.out.println("Enter your savings account balance ");
+        int savings = scan.nextInt();
+        System.out.println("Your savings balance is  " + savings);
+
+        if (checking<1000 || savings>=1500){
+            System.out.println(".15 fee for written checks ");
+        } else {
+            System.out.println("No service fee charge");
+        }
+
+        
+    }
+}
