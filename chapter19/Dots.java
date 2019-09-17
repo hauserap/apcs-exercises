d7f4ae19864382f41ebf1a9eca23bbb8797ec1b3
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-17 12:51:09 -0600

    start dots

--- chapter19/Dots.java
+++ chapter19/Dots.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Dots{
     public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word1 = scan.nextLine();
+        System.out.println("your first word is  " + word1);
+        System.out.println("Enter your second word");
+        String word2 = scan.nextLine();
+        System.out.println("your second word is  " + word2);
+
+        String twoWords = word1 +"   " + word2;
+
+        System.out.println(twoWords);
+
+
+
         
     }
 }
