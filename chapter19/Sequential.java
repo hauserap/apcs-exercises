47bfd04e4059bdd60062b5640b63e2469d16fd74
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-17 12:19:03 -0600

    finish sequential

---
+++ chapter19/Sequential.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Sequential{
+    public static void main(String [] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter N");
+
+        int n = scan.nextInt();
+
+        if(n>1){
+            int sum = (n*(n+1))/2;
+
+            System.out.println(sum);
+        }
+
+
+
+
+    }
+}
