ab9187e497191764a825db58263716b873816398
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-23 12:10:40 -0600

    finihs dots

--- chapter19/Dots.java
+++ chapter19/Dots.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
-
 public class Dots{
-    public static void main(String [] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a word");
-        String word1 = scan.nextLine();
-        System.out.println("your first word is  " + word1);
-        System.out.println("Enter your second word");
-        String word2 = scan.nextLine();
-        System.out.println("your second word is  " + word2);
 
-        String twoWords = word1 +"   " + word2;
+    public static void main (String [] args){
 
-        System.out.println(twoWords);
+        Scanner enter = new Scanner(System.in);
+        System.out.println("Enter first word:");
+        String firstWord = enter.nextLine();
+        int first = firstWord.length();
 
+        System.out.println("Eneter second word:");
+        String secondWord = enter.nextLine();
+        int second = secondWord.length();
 
 
-        
-    }
+        int strTotalLength = second+first;
+        int totalLenght = 30-strTotalLength;
+        int amountODots = 0;
+
+        while(totalLenght>0){
+            totalLenght =totalLenght-1;
+            amountODots = amountODots +1;    
+        }
+
+        System.out.print(firstWord);
+        while(amountODots>0){
+            amountODots = amountODots-1;
+            System.out.print(".");
+        }
+        System.out.print(secondWord);
+
+    }   
 }
