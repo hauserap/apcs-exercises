70b3712352ee23a13957a1788bce182d8d10ce5d
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-24 12:45:27 -0600

    start sum

---
+++ chapter24/Sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        
+
+        for(int n = scan.nextInt() ; n<=11 ; n=+2 ){
+            System.out.println(n);
+            
+
+        }
+
+        
+    }
+}
