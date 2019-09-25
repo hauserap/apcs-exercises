134ee411ca2e00127b73b5f7a7fe1b75bdebfb01
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-25 12:38:15 -0600

    finish sum

--- chapter24/Sum.java
+++ chapter24/Sum.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
-
 public class Sum{
-    public static void main(String [] args){
+
+    public static void main (String[]args){
+
         Scanner scan = new Scanner(System.in);
-
-        System.out.println("Enter a number");
-
-        int start = 1;
-        
-
-        for(int n = scan.nextInt() ; n<11 ; n++){
-
-            System.out.println(n++);
-            
-
+        System.out.println("Insert n");
+        int n = scan.nextInt();
+        int oddSum=0;
+        double nS = Math.pow(n,n);
+        for(int odd = 1 ; odd<=n ; odd+=2){
+            oddSum+=odd;
+            System.out.println(odd); 
         }
-
-        
+        System.out.println("The sum is " + oddSum);
+        System.out.println("N^2 is " + nS);
     }
 }
