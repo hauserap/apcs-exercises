30d93699a74989d79419aa31a0404e330ad7b13e
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-25 13:11:23 -0600

    start sheep

---
+++ chapter24/Sheep.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sheep{
+    public static void main(String [] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of years");
+        double t = scan.nextInt();
+
+        double sheep = 220/(1 + 10*0.83*t);
+
+        System.out.println(sheep);
+
+    }
+    
+}
