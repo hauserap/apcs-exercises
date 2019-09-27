34b2a95613019c327e6840148e6d419a6ddebf14
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-27 12:33:49 -0600

    finish sheeeppp

--- chapter24/Divisors.java
+++ chapter24/Divisors.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Divisors{
     public static void main (String [] args){
+        Scanner scan = new Scanner(System.in);
+        double n = scan.nextInt();
+
         
+
+
+
     }
 }
