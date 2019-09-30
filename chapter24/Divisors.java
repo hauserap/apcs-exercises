a1a42924bea522f51df9a145ce277d600b277499
Alex Hauser <hauserap@s.dcsdk12.org>
2019-09-30 12:35:46 -0600

    finish divisors

--- chapter24/Divisors.java
+++ chapter24/Divisors.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
-
-public class Divisors{
-    public static void main (String [] args){
-        Scanner scan = new Scanner(System.in);
-        double n = scan.nextInt();
-
-        System.out.println("Enter a number");
-
-           while(n>0){
-               n--;
-               System.out.println(n);
-           }
-
-
-
-    }
+public class Divisors {
+  public static void main(String[] args) {
+    Scanner scan =new Scanner(System.in);
+    System.out.println("Enter the number:");
+    int number=scan.nextInt();
+    int n = 0;
+    
+         for(int i = 1; i <= number; i++) {
+             if (number % i == 0) {
+                 n = n + i; 
+             }
+             
+         
+         }
+         System.out.println("The Sum Of The Divisors is: ");
+         System.out.println(n);
+}
 }
