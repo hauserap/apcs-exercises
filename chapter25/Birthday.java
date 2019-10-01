8d175218b183f4c9b06b389e8d2c2eda77a34002
Alex Hauser <hauserap@s.dcsdk12.org>
2019-10-01 12:49:10 -0600

    start birthday

--- chapter25/Birthday.java
+++ chapter25/Birthday.java
@@ -1,28 +1,14 @@
+import java.util.Scanner;
+
 public class Birthday{
-public static void main(String[] args){
-    int[] birthday = new int[200];
+    public static void main(String [] args){
 
-    for(int i = 0; i<20;i++){
-        Collision(birthday); 
+        Scanner scan = new Scanner(System.in);
+
+        
+        
+
+        
+
     }
-}
-
-public static int Collision(int birthday[]){
-    Random rand = new Random();  
-    for(int i = 1; i<birthday.length;i++){        
-        birthday[i] = rand.nextInt(365);
-    }
-
-    int count = 0;        
-    for(int i = 0; i<birthday.length; i++){          
-        for(int j= i+1 ; j<birthday.length; j++){            
-            if (birthday[i] == birthday[j]){               
-                count++;
-            }            
-        }          
-    }
-
-    System.out.print(count+" ");        
-    return count;  
-}
 }
