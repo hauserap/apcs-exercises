7d034044d65ca637d342830fd477ad5013fa0234
Alex Hauser <hauserap@s.dcsdk12.org>
2019-10-02 12:12:56 -0600

    finish password

--- chapter30/Password.java
+++ chapter30/Password.java
@@ -7,14 +7,14 @@ public class Password {
         Random random = new Random();
 
         System.out.println("Enter password 5 letters or less in all lowercase");
-        String usrPass = scan.nextLine();
+        String password = scan.nextLine();
 
         String guess = "";
 
         String chara = "abcdefghijklmnopqrstuvwxyz ";
         int attempts;
         int t = 0;
-        for (attempts = 0; !guess.equals(usrPass); attempts++) {
+        for (attempts = 0; !guess.equals(password); attempts++) {
             guess = "";
             for(t = 0; t < 5; t++) {
 
