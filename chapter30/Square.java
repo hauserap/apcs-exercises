196ac5a77b4acffb7878363a3c6a74dd98f8faba
Alex Hauser <hauserap@s.dcsdk12.org>
2019-10-02 17:02:19 -0600

    finish sqaure

--- chapter30/Square.java
+++ chapter30/Square.java
@@ -11,11 +11,33 @@ public class Square{
         String user2 = scan.nextLine();
         System.out.println("User 1 is " + user1 + " User 2 is " + user2 );
 
-        System.out.println("you will play 3 rounds. Good luck!");
+        System.out.println("how many rounds?");
 
-        double number = random.nextInt(100)+1;
+        for(int rounds= scan.nextInt(); rounds>0; rounds--){
+            double number = random.nextInt(100)+1;
+            System.out.println("what is the square root of  " + number);
+            double answer = Math.sqrt(number);
+            System.out.println("what is your answer  " + user1 +" ?");
+            double answer1 = scan.nextDouble();
+            System.out.println("what is your answer  " + user2 +" ?");
+            double answer2 = scan.nextDouble();
+            int points1 = 0;
+            int points2 = 0;
+            if(Math.abs(answer1-answer) <= Math.abs(answer2-answer)){
+                System.out.println(user1 + " is closer! " + user1 +" gets the point!");
+                points1=points1+1;
+            } else{
+                System.out.println(user2 + " Is closer  " +user2 + " gets the point!");
+                points2=points2+1;
+            }
+            System.out.println("current score: " + user1 + " has " + points1 + "   " +user2 + "  has " + points2);
 
-        System.out.println("What is the square root of " + number );
+
+
+           
+           
+        }
+       
         
     }
 }
