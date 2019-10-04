617d19c9bf66a92641b20ddbd00569f842198f98
Alex Hauser <hauserap@s.dcsdk12.org>
2019-10-04 12:23:54 -0600

    finihs pigs

--- chapter30/Pigs.java
+++ chapter30/Pigs.java
@@ -7,10 +7,19 @@ public class Pigs{
         Random random = new Random();
         int cpuPoints = 0;
         int dice = random.nextInt(18)+1;
-        int rollsRemaining = 3;
-        for(int playerPoints = 0; playerPoints<100 && rollsRemaining>0; rollsRemaining--){
+        int cpuDice = random.nextInt(18)+1;
+        int playerPoints = 0;
+        
+        for(int rollsRemaining = 3; playerPoints<100 && rollsRemaining>0; rollsRemaining--){
             playerPoints+=dice;
             System.out.println("The player has rolled " + dice + " points");
+            playerPoints = playerPoints + dice;
+            System.out.println("players has " + playerPoints);
+            System.out.println("computer rolled " + cpuDice + " points");
+            cpuPoints = cpuPoints + dice;
+            System.out.println("computer has " + cpuPoints);
+            
+
 
         }
     }
