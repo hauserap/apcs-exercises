eac4c97aaba2b92f85464c1fd0e341a25c737ecd
Alex Hauser <hauserap@s.dcsdk12.org>
2019-10-04 12:43:43 -0600

    start farkle

---
+++ project/Farkle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Farkle{
+    public static void main(String [] args){
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+        
+    
+    }
+}
