b376b1e6a04786acfc39ccd37ea76ed70b2064d3
Alex Hauser <hauserap@s.dcsdk12.org>
2019-10-28 13:20:51 -0600

    cone

---
+++ chapter47/Conespecs.java
@@ -0,0 +1,15 @@
+public class Conespecs{
+    private double height ;
+    private double width ;
+    private double slant;
+    private double angle;
+
+    public Cone (double height, double width, double slant, double angle){
+        this.height = height;
+        this.width = width;
+        this.slant = slant;
+        this.angle = angle;
+
+    }
+
+}
