d08fe1d8d18087f1fd300fd7ccbe8da3b6baeb8a
Alex Hauser <hauserap@s.dcsdk12.org>
2019-10-29 12:51:52 -0600

    cones

--- chapter47/Cone.java
+++ chapter47/Cone.java
@@ -1,5 +1,7 @@
 public class Cone{
     public static void main(String [] args){
-        
+        Cone cone1(12, 4,6, 90);
+
+        double calculateArea()
     }
 }
--- chapter47/Conespecs.java
+++ chapter47/Conespecs.java
@@ -1,14 +1,35 @@
 public class Conespecs{
     private double height ;
-    private double width ;
+    private double radius ;
     private double slant;
     private double angle;
 
-    public Cone (double height, double width, double slant, double angle){
+    public Cone (double height, double radius, double slant, double angle){
         this.height = height;
-        this.width = width;
+        this.radius = radius;
         this.slant = slant;
         this.angle = angle;
+
+
+        public double getHeight(){
+            return this.height;
+        }
+        public double getRadius(){
+            return this.radius;
+        }
+        public double getSlant(){
+            return this.slant;
+        }
+        public double getAngle(){
+            return this.angle;
+        }
+
+        public double calcualteArea(){
+           return (Math.PI* radius* (radius+Math.Sqrt((height*height)+(radius*radius))));
+        }
+
+
+
 
     }
 
