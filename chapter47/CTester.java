33cbb06d64f9cd207404ded5d96b4c42f2af2992
Alex Hauser <hauserap@s.dcsdk12.org>
2019-11-05 12:28:09 -0700

    finish cylinders

--- chapter47/CTester.java
+++ chapter47/CTester.java
@@ -1,5 +1,14 @@
 public class CTester{
     public static void main(String [] args){
-        
+        Cylinder a = new Cylinder ("a", 15.1,16.9);
+
+        String name = a.getName();
+        System.out.println("the cylinder is cylinder " + name);
+        double height = a.getHeight();
+        System.out.println("the height is  " + height);
+        double radius = a.getRadius();
+        System.out.println("the radius is "+ radius);
+        double volume = a.getVolume();
+        System.out.println("the volume is " + volume);
     }
 }
--- chapter47/Cylinder.java
+++ chapter47/Cylinder.java
@@ -1,12 +1,31 @@
 public class Cylinder{
 
+    private String name;
     private double height;
     private double radius;
 
 
-    public cylinders (double height. double radius){
+    public Cylinder (String name, double height , double radius){
+        this.name = name;
         this.height = height;
         this.radius = radius;
     }
+
+    public String getName(){
+        return this.name;
+    }
+    public double getHeight(){
+        return this.height;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public double getVolume(){
+       return Math.PI*(this.radius * this.radius)*this.height;
+    }
+
+    
 
 }
