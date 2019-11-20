e420e465c1df2cb5bf16c52b26791556a30e7ff1
Alex Hauser <hauserap@s.dcsdk12.org>
2019-11-20 12:32:33 -0700

    start flag

---
+++ chapter54/Flag.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Flag{
+    public static void main (String [] args){
+        while(1>0){
+            System.out.println("      t");
+            System.out.println("     a");
+            System.out.println("    y");
+            System.out.println("   l");
+            System.out.println("  e");
+            System.out.println(" r");
+            System.out.println("  ");
+            System.out.println(" t");
+            System.out.println("  a");
+            System.out.println("   y");
+            System.out.println("    l");
+            System.out.println("     e");
+            System.out.println("      r");
+            System.out.println("   ");
+        }
+    }
+}
