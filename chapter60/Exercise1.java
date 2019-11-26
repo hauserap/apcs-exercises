3cc3d2a1403e64b2835a50d1beb1d9982e2f3512
Alex Hauser <hauserap@s.dcsdk12.org>
2019-11-26 11:40:57 -0700

    finish exercise 2

---
+++ chapter60/Exercise1.java
@@ -0,0 +1,12 @@
+public class Exercise1
+{
+  public static void main ( String[] args )
+  {
+    int[] val = {0, 1, 2, 3}; 
+
+    int sum = 6;
+ 
+    System.out.println( "Sum of all numbers = " + sum );
+ 
+   }
+}
