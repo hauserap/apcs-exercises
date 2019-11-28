7fd86626c1318d23747ca4e9cd3bbccdff2747d5
Alex Hauser <hauserap@s.dcsdk12.org>
2019-11-27 21:37:15 -0700

    :finish chapter60

--- chapter60/Exercise3.java
+++ chapter60/Exercise3.java
@@ -4,7 +4,7 @@ public class Exercise3
   {
     int[] valA   = { 13, -22,  82,  17}; 
     int[] valB   = {-12,  24, -79, -13};
-    int[] sum    = {  0,   0,   0,   0};
+    int[] sum    = {  1,   2,   3,   4};
     
     // Add values from corresponding cells of valA and valB
     // and put the result in the corresponding cell of sum.
